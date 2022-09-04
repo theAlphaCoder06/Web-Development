890598c2686cea3140b2b43511add495f3299bd8
Shubham <shubhamsinghmau2@gmail.com>
2022-09-04 12:44:15 +0530

    Renamed some folders

--- 09. Dice Challenge/Dicee Challenge - Starting Files/js/index.js
+++ 09. Dice Challenge/Dicee Challenge - Starting Files/js/index.js
@@ -11,7 +11,10 @@ if(randomNumber1>randomNumber2)
 {
     document.querySelector("h1").innerHTML = "Player 1 wins.";
 }
-else
+else if(randomNumber1<randomNumber2)
 {
     document.querySelector("h1").innerHTML = "Player 2 wins.";
 } 
+else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
