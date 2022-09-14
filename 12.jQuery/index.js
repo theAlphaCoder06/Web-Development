5b409ea553ff6f6b13d626302a476c27a7e3906d
Shubham <shubhamsinghmau2@gmail.com>
2022-09-14 22:59:44 +0530

    jQuery files

---
+++ 12.jQuery/index.js
@@ -0,0 +1,16 @@
+//  document.querySelector("button").style.color = "red"; //JavaScript
+//$("h1").css("color", "yellow");    //jQuery
+
+//  if JS and jQuery is placed in head section
+// $(document).ready(function(){
+//     $("h1").css("color", "red");
+//   });
+
+$("h1").addClass("big-title margin-50");
+// $("h1").removeClass("big-title");
+
+// $("h1").text("bye");
+// $("button").text("Don't click me");
+// $("button").html("<em>CLICK ME PLZ</em>"); //jQuery alternative of innerHTML
+
+$("h1").attr("href","https://www.bing.com/?FORM=Z9FD1");
