17c4ed9bcd523f23321b09a0a3ff596eb6be569c
Shubham <shubhamsinghmau2@gmail.com>
2022-09-15 12:57:58 +0530

    jQuery

--- 12.jQuery/index.js
+++ 12.jQuery/index.js
@@ -13,4 +13,44 @@ $("h1").addClass("big-title margin-50");
 // $("button").text("Don't click me");
 // $("button").html("<em>CLICK ME PLZ</em>"); //jQuery alternative of innerHTML
 
-$("h1").attr("href","https://www.bing.com/?FORM=Z9FD1");
+// $("h1").attr("href","https://www.bing.com/?FORM=Z9FD1");
+
+// $("h1").click(function(){
+//     $("h1").css("color", "purple");
+// });
+
+// Event Listener using javascript
+
+// for(let i = 0; i<document.querySelectorAll("button").length;i++){
+//     document.querySelectorAll("button")[i].addEventListener("click",function(){
+//         document.querySelector("h1").style.color = "red";
+//     });
+// }
+
+// Event listener using jQuery
+
+// $("button").click(function(){
+//     $("h1").css("color", "purple");
+// });
+
+// $("body").keypress(function(event){
+//     console.log(event.key);
+// });
+
+// $("body").keypress(function(event){
+//     $("h1").text(event.key);
+// });
+
+// $("h1").on("click", function(){
+//     $("h1").css("color", "purple");
+// });
+
+// $("h1").before("<button>New</button>");
+
+
+// $("button").remove();
+
+$("body").on("keypress", function(){
+    $("h1").slideUp().slideDown().animate({opacity:0.5});
+});
+
