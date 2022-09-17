8ea1c56b2b35dbe6236221f39d8f2ced2cff73e5
Shubham <shubhamsinghmau2@gmail.com>
2022-09-17 22:40:30 +0530

    Added visual effect and sound when clicked on coloured tiles

--- 13. The Simon Game/game.js
+++ 13. The Simon Game/game.js
@@ -2,19 +2,34 @@ let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickPattern = [];
 
+$(".btn").click(function(){
+    let userChosenColour = $(this).attr("id");
+    userClickPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
 
 function nextSequence(){
 
-    let randomNumber = Math.floor(Math.random()*3)+1;
+    let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
     $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    let audio = new Audio("sounds/" + randomChosenColor +".mp3");
+    playSound(randomChosenColor);
+}
+
+function playSound(name){
+    let audio = new Audio("sounds/" + name +".mp3");
     audio.play();
 }
 
-function clickHandler(){
-    
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
 }
