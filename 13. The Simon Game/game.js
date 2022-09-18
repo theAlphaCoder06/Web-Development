91a08a65179d7c46e32f2f29bbf314131acde3e3
Shubham <shubhamsinghmau2@gmail.com>
2022-09-18 11:12:01 +0530

    Completed the game

--- 13. The Simon Game/game.js
+++ 13. The Simon Game/game.js
@@ -1,20 +1,64 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickPattern = [];
+let started = false;
+let level = 0;
+
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level "+ level);     
+        nextSequence();
+        started = true;
+    }
+    
+});
 
 $(".btn").click(function(){
     let userChosenColour = $(this).attr("id");
     userClickPattern.push(userChosenColour);
+    console.log("userClickPattern " +userClickPattern);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickPattern.length-1);
 });
 
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickPattern[currentLevel]){
+        console.log("success");
+        if(userClickPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else{
+        console.log("fail");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        $("h1").text("Game Over, Press any key to restart.");
+        playSound("wrong");
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
 
 function nextSequence(){
 
+    userClickPattern = [];
+    level++;
+    $("#level-title").text("Level "+ level);
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    console.log("gamePattern " + gamePattern);
 
     $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
@@ -33,3 +77,4 @@ function animatePress(currentColor){
       $("#" + currentColor).removeClass("pressed");
     }, 100);
 }
+
