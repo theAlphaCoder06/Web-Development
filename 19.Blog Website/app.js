7fa69cd4faed9a1922bf04d4c765d22f58bc8e2c
Shubham <shubhamsinghmau2@gmail.com>
2022-10-01 23:33:58 +0530

    Added dynamic pages

--- 19.Blog Website/app.js
+++ 19.Blog Website/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,6 +11,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const posts = [];
 
 const app = express();
+app.locals._ = _;
 
 app.set('view engine', 'ejs');
 
@@ -17,7 +19,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/posts/:postName", (req, res)=>{
+  const requestedTitle = _.lowerCase(req.params.postName)
 
+  //! Purana tarika hai
+  //! for(let i = 0; i<posts.length; i++){
+  //!   if(requestedTitle === posts[i].title){
+  //!     console.log("Match found!")
+  //!   }
+  //! }
+
+  //? Naya tareeeka hai
+
+  posts.forEach((post)=>{
+    const storedTitle = _.lowerCase(post.title);
+    const storedBody = post.body;
+    if(storedTitle === requestedTitle){
+      res.render("post", {storedTitle: storedTitle, storedBody: storedBody});
+    }
+  });
+
+})
 
 app.get("/", (req, res)=>{
   res.render("home", {homeContent: homeStartingContent, posts: posts});
