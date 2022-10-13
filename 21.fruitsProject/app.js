d0a10db8cd3ba71ffe6d03e5cf26d295b68b9ee3
Shubham <shubhamsinghmau2@gmail.com>
2022-10-13 12:27:18 +0530

    Added data validation

--- 21.fruitsProject/app.js
+++ 21.fruitsProject/app.js
@@ -9,56 +9,25 @@ async function main(){
     useUnifiedTopology: true
   });
 
-  // ? Creating new schema
-  
   const fruitSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+    name: {
+      type: String,
+      required: [true, 'Why no name?']
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 10
+    },
     review: String
   });
   const Fruit = mongoose.model("Fruit", fruitSchema);//mongoose converts the 'Fruit" to 'fruits' to name our collection and this is done using _lodash
-  const fruit = new Fruit ({
-    name: "Apple",
-    rating: 7,
-    review: "Pretty solid"
-  });
-
-  const kiwi = new Fruit ({
-    name: "Kiwi",
-    rating: 10,
-    review: "The best"
-  })
-  const orange = new Fruit ({
-    name: "Orange",
-    rating: 9,
-    review: "OK OK!!"
-  })
-  const banana = new Fruit ({
-    name: "Banana",
-    rating: 8,
-    review: "Good"
-  })
-  // await kiwi.save();
-  // await orange.save();
-  // await banana.save();
-
-  Fruit.insertMany([kiwi, orange, banana], (err)=>{
-    if(err)
-      console.log(err);
-    else
-      console.log("Success")
-  })
 
   const peopleSchema = new mongoose.Schema({
     name: String,
     age: Number
   })
   const People = mongoose.model("People", peopleSchema);
-  const people = new People({
-    name: "Shubham",
-    age: 19
-  })
-  await people.save();
-  
-  //await fruit.save() // whenever we run this it will save the same apple to the database
+
+  mongoose.connection.close();
 }
