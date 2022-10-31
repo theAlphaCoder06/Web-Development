8c59948fb932e12509c7cdbeb143e2bae82db641
Shubham <shubhamsinghmau2@gmail.com>
2022-10-31 19:58:40 +0530

    Level 3 Authentication with environment variables

--- 26.Authentication/app.js
+++ 26.Authentication/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -9,7 +10,7 @@ const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-const secret = "Thisisourlittlesecret";
+const secret = process.env.SECRET;
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 const userSchema = new mongoose.Schema({
     email: String,
