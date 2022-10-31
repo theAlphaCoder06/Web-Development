f0c2d5a8ba780a78def1e91aba6e385db48cb8ef
Shubham <shubhamsinghmau2@gmail.com>
2022-10-31 19:43:25 +0530

    Level 2 Authentication with npm mongoose encryption

--- 26.Authentication/app.js
+++ 26.Authentication/app.js
@@ -2,18 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const port = 3000;
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
+const secret = "Thisisourlittlesecret";
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+});
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]})
 const User = new mongoose.model("User", userSchema);
 
 app.get('/', (req, res)=>{
@@ -54,8 +56,8 @@ app.post('/login', (req, res)=>{
                 }
             }
         }
-    });
-});
+    })
+})
 
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`);
